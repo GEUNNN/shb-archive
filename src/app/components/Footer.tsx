81b41460f07857606597e68a53e48c130ef3ceae
GEUNNN <geunhwa.lee.01@gmail.com>
2025-02-04 22:35:06 +0900

    [SHBA-12] feat: Footer 추가

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,55 @@
+"use client";
+
 import React, { FC } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+interface FooterItemProps {
+  menu: string;
+  href: string;
+  icon: string;
+  isActive: boolean;
+}
+
+const FooterItem = ({ menu, href, icon, isActive }: FooterItemProps) => {
+  const iconPath = isActive
+    ? `/assets/${icon}-selected.svg`
+    : `/assets/${icon}.svg`;
+
+  console.log("isActive? >>>>>>", icon, isActive);
+
+  return (
+    <li>
+      <Link href={href} className="flex flex-col items-center">
+        <Image src={iconPath} alt={icon} width={24} height={24} />
+        <span className="text-xs">{menu}</span>
+      </Link>
+    </li>
+  );
+};
 
 const Footer: FC = () => {
+  const pathname = usePathname();
+  console.log("current pathname >>>>>>", pathname);
+  const menuItems = [
+    { name: "HOME", href: "/", icon: "home" },
+    { name: "PHOTOS", href: "/photos", icon: "photos" },
+    { name: "VIDEOS", href: "/videos", icon: "videos" },
+  ];
+
   return (
-    <footer className="absolute bottom-0 left-0 w-full">
+    <footer className="absolute bottom-0 left-0 w-full px-5 py-2 bg-white shadow-md">
       <ul className="flex justify-between">
-        <li>Home</li>
-        <li>Photo</li>
-        <li>Video</li>
+        {menuItems.map((item, idx) => (
+          <FooterItem
+            key={idx}
+            href={item.href}
+            menu={item.name}
+            icon={item.icon}
+            isActive={pathname === item.href}
+          />
+        ))}
       </ul>
     </footer>
   );
