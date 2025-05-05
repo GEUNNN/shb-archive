687a708032b9e61176d78859992fdc14651a6f4a
GEUNNN <geunhwa.lee.01@gmail.com>
2025-05-05 21:46:15 +0900

    SHBA-15 feat: video component

---
+++ src/app/videos/components/videoItem.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import React, { FC } from "react";
+import Image from "next/image";
+
+interface VideoItemProps {
+  title: string;
+  thumbnail: string;
+  date: string;
+}
+
+const videoItem: FC<VideoItemProps> = ({ title, thumbnail, date }) => {
+  console.log("videoItem");
+  return (
+    <Card
+      className="w-full border-transparent shadow-none"
+      onClick={() => console.log("clicked")}
+    >
+      <div className="relative w-full aspect-video">
+        <Image src={thumbnail} alt={title} fill className="object-cover" />
+      </div>
+      <CardTitle className="text-lg font-semibold">{title}</CardTitle>
+      <CardDescription>{date}</CardDescription>
+    </Card>
+  );
+};
+
+export default videoItem;
--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,7 +1,46 @@
 import React, { FC } from "react";
+import VideoItem from "./components/videoItem";
 
 const Video: FC = () => {
-  return <div>Video</div>;
+  const videos = [
+    {
+      title: "focused cam",
+      thumbnail:
+        "https://i.ytimg.com/vi/3JLV992Zxbo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDOtfHnlSW2CfoZ11FRcEWnb0b-xw",
+      date: "",
+    },
+    {
+      title: "focused cam",
+      thumbnail:
+        "https://i.ytimg.com/vi/3JLV992Zxbo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDOtfHnlSW2CfoZ11FRcEWnb0b-xw",
+      date: "",
+    },
+    {
+      title: "focused cam",
+      thumbnail:
+        "https://i.ytimg.com/vi/3JLV992Zxbo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDOtfHnlSW2CfoZ11FRcEWnb0b-xw",
+      date: "2025-05-01",
+    },
+    {
+      title: "focused cam",
+      thumbnail:
+        "https://i.ytimg.com/vi/Za75TeSyV3s/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB-8OacGnCVuwXomlxaYrTGj7hmxQ",
+      date: "2025-05-01",
+    },
+  ];
+
+  return (
+    <div className="px-1 grid grid-cols-1 gap-4">
+      {videos.map((item, index) => (
+        <VideoItem
+          key={index}
+          title={item.title}
+          thumbnail={item.thumbnail}
+          date={item.date}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Video;
