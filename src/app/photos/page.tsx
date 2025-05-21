d8f4c293101aea728d5defea9e7a7e4d192194d1
GEUNNN <geunhwa.lee.01@gmail.com>
2025-05-21 20:57:42 +0900

    SHBA-14 feat: add filter for photos page

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,11 +1,30 @@
+"use client";
 import React, { FC } from "react";
 import { PhotoItem } from "./components/PhotoItem";
+import { Badge } from "@/components/ui/badge";
+
+const filter = [
+  { type: "인스타", value: "인스타" },
+  { type: "멤트", value: "멤트" },
+  { type: "플챗", value: "플챗" },
+];
 
 const Photo: FC = () => {
   const arr = [1, 2, 3, 4, 5];
 
   return (
-    <div>
+    <div className="p-2 grid grid-cols-1 gap-4">
+      <div className="px-1 flex justify-end gap-3 sticky top-0 bg-white z-10 py-2">
+        {filter.map((item) => (
+          <Badge
+            key={item.type}
+            className="bg-[#F09884] text-white"
+            onClick={() => console.log(item.value)}
+          >
+            {item.type}
+          </Badge>
+        ))}
+      </div>
       {arr.map((item, idx) => (
         <PhotoItem key={idx} />
       ))}
